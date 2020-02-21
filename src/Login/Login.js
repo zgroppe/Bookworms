48bcb0b646f2c85a3bd394e8c53dfda0ae2da5a1
Zackary Groppe <zackgroppe@hotmail.com>
2020-02-21 10:54:28 -0600

    test

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -12,7 +12,7 @@ export default function Login(props) {
 				{/* <h1>Login</h1> */}
 				<TitleText>Login CHANGE</TitleText>
 				<SubtitleText>
-					Clock-in with your username<br />Log-in with your username and password
+					12345 Clock-in with your username<br />Log-in with your username and password
 				</SubtitleText>
 			</div>
 			<div style={{ flexDirection: 'column', display: 'flex' }}>
