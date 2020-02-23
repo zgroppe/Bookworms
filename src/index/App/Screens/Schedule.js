05349c97cebf90dcb0d63c302888a676fe612584
Zackary Groppe <zackgroppe@hotmail.com>
2020-02-23 16:17:40 -0600

    Style and other things added to calendar for full month view, and some example events

--- src/index/App/Screens/Schedule.js
+++ src/index/App/Screens/Schedule.js
@@ -3,9 +3,12 @@ import {Calendar, momentLocalizer, Views} from 'react-big-calendar'
 import moment from 'moment'
 import "react-big-calendar/lib/css/react-big-calendar.css"
 
+moment.locale("en");
 const localizer = momentLocalizer(moment)
 let allViews = Object.keys(Views).map(k => Views[k])
-var myEventsList = [{'title': 'Meeting','allDay': false, 'start': new Date(2020, 2, 23, 2, 22, 0), 'end': new Date(2020, 2, 24, 2, 22, 0)}, {'title': 'Meeting','allDay': false, 'start': new Date(2020, 2, 25, 1, 11, 0), 'end': new Date(2020, 2, 26, 1, 11, 0)}]
+
+//Date values are supposed to be one less than the actual day, given that date info is probably held in some kind of array
+var myEventsList = [{'title': 'Meeting','allDay': false, 'start': new Date(2020, 1, 23, 2, 22, 0), 'end': new Date(2020, 1, 24, 2, 22, 0)}, {'title': 'Meeting','allDay': false, 'start': new Date(2020, 1, 25, 1, 11, 0), 'end': new Date(2020, 1, 26, 1, 11, 0)}]
 
 export default function Schedule(props) {
 	return (
@@ -13,6 +16,7 @@ export default function Schedule(props) {
 			<h1>Schedule</h1>
 			<h3>This is some schedule content</h3> 
 			<Calendar
+				style={{ height: '80vh', width: '80vw' }}
 				events={myEventsList}
 				views={allViews}
 				localizer={localizer}
