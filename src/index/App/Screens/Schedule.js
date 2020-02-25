3347a7ef8af591a92cebe2ca7916af8e8f4ca11f
david marselus <marcellloki@gmail.com>
2020-02-24 19:15:11 -0600

    added onClick
    timeslots and steps need readjustment

--- src/index/App/Screens/Schedule.js
+++ src/index/App/Screens/Schedule.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import '../Styles/Schedule.css';
 moment.locale('en');
 const localizer = momentLocalizer(moment);
-let allViews = Object.keys(Views).map((k) => Views[k]);
-
-//Date values are supposed to be one less than the actual day, given that date info is probably held in some kind of array
-var myEventsList = [
-	{ title: 'Meeting', allDay: false, start: new Date(2020, 1, 23, 2, 22, 0), end: new Date(2020, 1, 24, 2, 22, 0) },
-	{ title: 'Meeting', allDay: false, start: new Date(2020, 1, 25, 1, 11, 0), end: new Date(2020, 1, 26, 1, 11, 0) }
-];
 
 export default function Schedule(props) {
+	const [ myEventsList, setMyEventsList ] = useState([]);
+
+	//useEffect => what to do after the component is rendered
+	useEffect(() => {
+		//calling setMyEventsList to set hardcoded list
+		setMyEventsList([
+			{ title: 'Employee 1', start: new Date(2020, 1, 23, 10), end: new Date(2020, 1, 23, 18) },
+			{ title: 'Employee 3', start: new Date(2020, 1, 25, 10), end: new Date(2020, 1, 25, 16) }
+		]);
+	}, []);
+	const handleSelect = ({ start, end }) => {
+		const title = window.prompt('New Event name');
+		if (title) {
+			setMyEventsList([ ...myEventsList, { title, start, end } ]);
+		}
+	};
 	return (
 		<div>
 			<h1>Schedule</h1>
 			<h3>This is some schedule content</h3>
 			<Calendar
-				style={{ height: '80vh', width: '80vw', margin: '10vw' }}
-				events={myEventsList}
-				views={allViews}
-				defaultView={'week'}
+				selectable
 				localizer={localizer}
-				step={60}
-				allDayAccessor={false}
-				showMultiDayTimes
-				// startAccessor="start"
-				// endAccessor="end"
+				events={myEventsList}
+				views={[ 'month', 'week' ]}
+				defaultView={Views.WEEK}
+				defaultDate={new Date(2020, 1, 25)}
+				onSelectEvent={(event) => alert(event.title)}
+				onSelectSlot={handleSelect}
+				style={{ height: '80vh', width: '80vw', margin: '10vw' }}
 			/>
 		</div>
 	);
