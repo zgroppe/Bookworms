e30ef905838621b6a1d5fd692ba6950bc01eab3f
Jonie Moya <moyajj172@gmail.com>
2020-03-30 01:40:16 -0500

    Account Page Layout Change
    I tried to align with how you described it jacob but if its not what you had in mind lmk

--- src/index/App/Screens/Account.js
+++ src/index/App/Screens/Account.js
@@ -40,31 +40,36 @@ export default function Account(props) {
 
     return (
         <div>
-            <h1>Account</h1>
-            <h1>{data.getUserByID.firstName}</h1>
-            <h3>
+            <h1 style={{ textAlign: 'left' }}>
+            Account {data.getUserByID.firstName}
+            </h1>
+            <h3 style={{ textAlign: 'left' }}>
                 Here you can update your account information. Please provide
                 your FirebaseID in the box below before submitting any changes
             </h3>
-
-            <button onClick={() => refetch()}>Click me!</button>
-
-            <h4>Provide your FirebaseID here</h4>
+            <h4 style={{ textAlign: 'left' }}>Provide your FirebaseID here</h4>
             <TextInput
+                style={{ float: 'left'}}
                 placeholder='FirebaseID'
                 type='text'
                 value={FirebaseID}
                 onChange={e => ValidateFirebaseID(e.target.value)}
             />
-
-            <h4>Update your email here</h4>
+            
+            <PrimaryButton style={{ clear: 'left', float: 'left', width: '100px' }}
+            onClick={() => refetch()}>Submit
+            </PrimaryButton>
+            
+            <h4 style={{ clear: 'left', textAlign: 'left' }}>Update your email here</h4>
             <TextInput
+                style={{ float: 'left' }}
                 placeholder='Email'
                 type='text'
                 value={updatedEmail}
                 onChange={e => updateEmail(e.target.value)}
             />
             <PrimaryButton
+                style={{ clear: 'left', float: 'left' }}
                 onClick={() => {
                     auth.login(() => {
                         props.history.push(Screens[0].path)
@@ -74,14 +79,16 @@ export default function Account(props) {
                 Update Email
             </PrimaryButton>
 
-            <h4>Update your First and Last Name here</h4>
+            <h4 style={{ clear: 'left', textAlign: 'left' }}>Update your First and Last Name here</h4>
             <TextInput
+                style={{ float: 'left' }}
                 placeholder='First Name'
                 type='text'
                 value={updatedFName}
                 onChange={e => updateFName(e.target.value)}
             />
             <PrimaryButton
+                style={{ clear: 'left', float: 'left' }}
                 onClick={e =>
                     update({
                         variables: {
@@ -94,12 +101,14 @@ export default function Account(props) {
                 Update First Name
             </PrimaryButton>
             <TextInput
+                style={{ clear: 'left',float: 'left' }}
                 placeholder='Last Name'
                 type='text'
                 value={updatedLName}
                 onChange={e => updateLName(e.target.value)}
             />
             <PrimaryButton
+                style={{ clear: 'left', float: 'left' }}
                 onClick={() => {
                     auth.login(() => {
                         props.history.push(Screens[0].path)
@@ -109,14 +118,16 @@ export default function Account(props) {
                 Update Last Name
             </PrimaryButton>
 
-            <h4>Update your shift preferences here</h4>
+            <h4 style={{ clear: 'left', textAlign: 'left' }}>Update your shift preferences here</h4>
             <TextInput
+                style={{ float: 'left' }}
                 placeholder='Days'
                 type='text'
                 value={updatedDays}
                 onChange={e => updateDays(e.target.value)}
             />
             <TextInput
+                style={{ clear: 'left', float: 'left' }}
                 placeholder='Starting Hour'
                 type='text'
                 value={updatedSHour}
@@ -124,25 +135,28 @@ export default function Account(props) {
             />
 
             <TextInput
+                style={{ clear: 'left', float: 'left' }}
                 placeholder='Ending Hour'
                 type='text'
                 value={updatedEHour}
                 onChange={e => updateEHour(e.target.value)}
             />
             <TextInput
+                style={{ clear: 'left', float: 'left'}}
                 placeholder='Color on calendar'
                 type='text'
                 value={updatedSColor}
                 onChange={e => updateSColor(e.target.value)}
             />
             <PrimaryButton
+            style={{ clear: 'left', float: 'left' }}
                 onClick={() => {
                     auth.login(() => {
                         props.history.push(Screens[0].path)
                     })
                 }}
             >
-                Update Hour Preferences
+            Update Hour Preferences
             </PrimaryButton>
         </div>
     )
