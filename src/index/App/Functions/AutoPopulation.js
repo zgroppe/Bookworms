1905186b94ac3aa1333c3342294b322d06d56932
david marselus <marcellloki@gmail.com>
2020-03-31 22:00:23 -0500

    Just a text for future work

---
+++ src/index/App/Functions/AutoPopulation.js
@@ -0,0 +1,23 @@
+
+/*
+        Per Shift
+*/
+//InClass - -100
+//Unpreferred - -1
+//Neutral - -0
+//Preferred - 1
+
+/*
+        Per Employee
+*/
+//Employee value - [0-4]
+
+
+/*
+        Special Case
+*/
+//inBetween Class && Preferred - 2
+//inBetween Class && Neutral - 1
+
+
+
